4153dd043f1a3bd2e339b4974aa9980c321220f8
Olga Pshenichnikova <ya.olgapshenichnikova@yandex.ru>
2020-03-12 14:35:26 +0200

    Events;

--- 2DKosh/PolyCtl.h
+++ 2DKosh/PolyCtl.h
@@ -85,6 +85,7 @@ END_CONNECTION_POINT_MAP()
 BEGIN_MSG_MAP(CPolyCtl)
 	CHAIN_MSG_MAP(CComControl<CPolyCtl>)
 	DEFAULT_REFLECTION_HANDLER()
+	MESSAGE_HANDLER(WM_LBUTTONDOWN, OnMouseLDown)
 END_MSG_MAP()
 // Handler prototypes:
 //  LRESULT MessageHandler(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled);
@@ -96,6 +97,29 @@ END_MSG_MAP()
 
 // IPolyCtl
 public:
+
+	LRESULT OnMouseLDown(UINT uMsg, WPARAM wParam, LPARAM lParam, BOOL& bHandled)
+	{
+		HRGN hRgn;
+		WORD xPos = LOWORD(lParam);  // horizontal position of cursor
+		WORD yPos = HIWORD(lParam);  // vertical position of cursor
+
+		CalcPoints(m_rcPos);
+
+		// Create a region from our list of points
+		hRgn = CreatePolygonRgn(&m_arrPoint[0], m_nSides, WINDING);
+
+		// If the clicked point is in our polygon then fire the ClickIn
+		//  event otherwise we fire the ClickOut event
+		if (PtInRegion(hRgn, xPos, yPos))
+			Fire_ClickIn(xPos, yPos);
+		else
+			Fire_ClickOut(xPos, yPos);
+
+		// Delete the region that we created
+		DeleteObject(hRgn);
+		return 0;
+	}
 
 	HRESULT OnDraw(ATL_DRAWINFO& di)
 	{
